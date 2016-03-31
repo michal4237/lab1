0497061c2a130462685f45fc6312ecd7091f36bb
student <student@debian>
2016-03-31 17:31:28 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -7,17 +7,32 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+	int n = atoi (argv[1]);
+	srand(argc==3? atoi(argv[2]): time(NULL));
+	drawCharSquare(charSquare(n), n);
+	printf("\n");	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char *[n];
+	
+	for (int i=0; i<n; i++) square[i] = new char[n];
+	
+
+	for (int j=0; j<n*n; j++) 
+		square[j/n][j%n] = 'a' + rand()%('z'-'a');
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+	for(int i=0; i<n*n; i++)
+		(i%n==0 && i!=0) ? printf("\n%c ", square[i/n][i%n]): printf("%c ", square[i/n][i%n]);
+
+	for (int j=0; j<n; j++) delete[] square[j];
+
+	delete[] square;
 
 }
